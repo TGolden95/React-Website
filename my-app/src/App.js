2d118f9db071c9b919e2c55714bfd96ca71d6e56
Triston Golden <triston_golden@yahoo.com>
2022-02-28 22:28:20 -0500

    about me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./Components/Hero";
 import "./assets/styles.css";
+import AboutMe from "./Components/AboutMe";
 
 export default function App() {
   return (
     <div>
       <Hero />
+      <AboutMe />
     </div>
   );
 }
---
+++ my-app/src/Components/AboutMe.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div class="page-wrapper">
+      <section class="page-section" id="about">
+        <h2>Background Story</h2>
+
+        <div class="flex-item">
+          <p>
+            Hello! My name is Triston Golden for those of you visiting this
+            webpage. As you read along throughout this webpage I will continue
+            to open up about myself and share with you some details that will
+            help you get to know me better. With that being said, I welcome you
+            to learn a little about me. I'm the oldest of five children, which
+            needless to say I try my best to lead by example. I graduated from
+            Shiloh High School in Gwinnett county. I did wrestling and football
+            year round. I got my first real job at my local McDonald's. During
+            my college days I loved to play Madden and NBA 2K as well as play
+            recreational basketball at my local LA Fitness. At one point I was a
+            top 1,500 Madden NFL player in the world on the playstation 4. I
+            can't forget about NBA 2K though! I very much enjoyed creating my
+            own myplayer build, as well as being yelled at by 3rd graders about
+            how bad I was at the game.
+          </p>
+        </div>
+      </section>
+
+      <section class="page-section" id="experience">
+        <h2>Experience</h2>
+        <div class="flex-item2">
+          <p>
+            As I mentioned earlier, my first job was at McDonald's. When I moved
+            to college I was able to essentially transfer to a closer McDonald's
+            in the city of Kennesaw.
+          </p>
+          <p>
+            During my time at Kennesaw State I was a full time student, and also
+            I worked for a local Homegoods and work study position. I pretty
+            much worked these two jobs throughout my entire tenure at Kennesaw
+            State.
+          </p>
+          <p>
+            During my second to last semester of college I was hired at AT&T,
+            where I worked for a little over 3 years.
+          </p>
+          <p>
+            I am currently enrolled in a coding bootcamp at Georgia Tech with an
+            end date of March 9th, 2022. I hope to take the skills I've aquired
+            during my bootcamp and find a job in a related field.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
