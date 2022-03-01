59bb00eb194e2d5f9e46fddf53ba013b9f6839d4
Triston Golden <triston_golden@yahoo.com>
2022-02-28 21:45:47 -0500

    added styles

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "./Components/Hero";
+import "./assets/styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
---
+++ my-app/src/Components/Hero.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Triston Golden</h1>
+        <nav>
+          <a href="#about">About Me</a>
+          <a href="#experience">Experience</a>
+          <a href="#work">Work</a>
+          <a href="#contact">Contact Me</a>
+          {/* <a href="./Assets/Golden Resume (Coding)- copy.pdf">Resume</a> */}
+          {/* <p><a href="https://github.com/TGolden95"><img src="./images/github logo.png" class="gitlogo"></a></p> */}
+        </nav>
+      </header>
+    </div>
+  );
+}
