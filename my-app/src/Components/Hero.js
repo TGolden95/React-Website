e5de1fe3b1bc80f6e903f1f479eddc7f95082535
Triston Golden <triston_golden@yahoo.com>
2022-02-28 22:05:58 -0500

    headshot

--- my-app/src/Components/Hero.js
+++ my-app/src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "../assets/headshot.png";
 
 export default function App() {
   return (
@@ -11,9 +12,24 @@ export default function App() {
           <a href="#work">Work</a>
           <a href="#contact">Contact Me</a>
           {/* <a href="./Assets/Golden Resume (Coding)- copy.pdf">Resume</a> */}
-          {/* <p><a href="https://github.com/TGolden95"><img src="./images/github logo.png" class="gitlogo"></a></p> */}
+          {/* <p><a href="https://github.com/TGolden95"><img src="./images/github logo.png" className="gitlogo"></a></p> */}
         </nav>
       </header>
+
+      <div className="detail">
+        <h1>
+          Hello, my name is Triston <span>Golden</span> and
+        </h1>
+        <p>Welcome to my professional portfolio...</p>
+
+        <div className="flex-box">
+          <img
+            src={Image}
+            // style="max-width:auto;height:auto;"
+            className="headshot"
+          />
+        </div>
+      </div>
     </div>
   );
 }
